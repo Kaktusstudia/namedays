3b9a9c80cd46705c54ecb91ffa5dd00cf0d31c78
Jondous <jonas.hubeny@gmail.com>
2026-02-19 22:20:01 +0100

    interacting with json file

--- svatky.py
+++ svatky.py
@@ -1,23 +1,38 @@
 #svatky
 from pyfiglet import Figlet
 import datetime
+import json
+
+with open("svatky-db.json", "r", encoding="utf-8") as f: 
+    data = json.load(f)         # nacte .json
+
+    today = datetime.datetime.now()
+    month_index = today.month - 1
+    day_key = f"{today.day}."
+
+    month_obj = data[month_index]
+    name = month_obj.get(day_key, "")
 
 
-def Main():
     f = Figlet(font='slant')
     print(f.renderText('ceske svatky'))
     
     t = datetime.datetime.now()
     print(t)
 
-    print("Kdo má svítek..")
-    print("dnes")
-    print("zitra")
+    print("Kdo má svátek")
+    print("1) DNES")
+    print("2) ZITRA")
 
-    a = input("vyber jednu z moznosti: \n")
+    a = input("vyber jednu z moznosti: ")
 
+    if a == "1":
+        if name:
+            print(f"Dnes má svátek {name}")
 
-    
+        else:
+            print("Spatna moznost!")
 
-
-Main()
+    if a == "2":
+        if name:
+            print()
